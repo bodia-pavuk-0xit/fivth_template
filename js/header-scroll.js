b36b95f3018c0116e248b27193beff85cd380782
bodia-pavuk-0xit <b.yakobchuk.workmail@gmail.com>
2023-04-27 18:21:01 +0300

    Made a footer and fixed some bugs with the header

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,15 +1,30 @@
 const navbar = document.querySelector('.navbar');
 const navbarToggle = document.querySelector('.navbar-toggler');
+const navbarOverlay = document.querySelector('.navbar-overlay');
 
-window.addEventListener('scroll', function() {
+function checkNavbarPosition() {
   if (window.pageYOffset > navbar.offsetHeight) {
     navbar.classList.add('navbar-bg');
   } else {
     navbar.classList.remove('navbar-bg');
+    if (navbar.classList.contains('active')) {
+      navbar.classList.add('navbar-bg');
+    } else {
+      navbar.classList.remove('navbar-bg');
+    }
   }
-});
+}
+
+window.setInterval(checkNavbarPosition, 1);
 
 navbarToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
   navbarOverlay.classList.toggle('active');
+  navbar.classList.toggle('navbar-bg');
 });
+
+checkNavbarPosition();  var url = window.location;
+$('ul.navbar-nav a[href="'+ url +'"]').parent().addClass('active');
+$('ul.navbar-nav a').filter(function() {
+  return this.href == url;
+}).parent().addClass('active');
